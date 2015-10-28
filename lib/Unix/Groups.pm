3328ede88ff136f6ffa5dd885ed5c05ea79f13b8
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-10-28 21:43:04 +0000

    Add the pod

--- lib/Unix/Groups.pm
+++ lib/Unix/Groups.pm
@@ -1,5 +1,92 @@
 use v6;
 
+=begin pod
+
+=head1 NAME
+
+Unix::Groups - access to details from C</etc/group>
+
+=head1 SYNOPSIS
+
+=begin code
+use Unix::Groups;
+
+my $groups = Unix::Groups.new;
+
+say "The logged in user is member of these groups:";
+
+for $groups.groups-for-user($*USER.Str) -> $group {
+	say $group.name;
+}
+=end code
+
+=head1 DESCRIPTION
+
+This module provides access to the group details from C</etc/group>, with
+similar to C<getgrent()>, C<getgrnam> and C<getgrgid> in the Unix standard
+C library.
+
+The methods either return a L<Unix::Groups::Group|#Unix::Groups::Group> object
+or an array of those objects.
+
+Because this module goes directly to the group file, if your system is
+configured to retrieve its group information from e.g. C<NIS> or C<LDAP>
+it may not necessarily reflect all the groups present, just the local ones.
+
+=head1 METHODS
+
+=head2 method groups
+
+    method groups() returns Array[User::Groups::Group]
+
+Returns the full list of groups, sorted in order of group id.
+
+=head2 method group-by-name
+
+    method group-by-name(Str $name) returns User::Groups::Group
+
+Returns the group specified by C<$name> or the type object if none exists.
+
+=head2 method group-by-id
+
+    method group-by-id(Int $id) returns User::Groups::Group 
+
+Returns the group specified by the integer group id of the type object if
+none exists.
+
+=head2 method groups-for-user
+
+    method groups-for-user(Str() $user) returns Array[User::Groups::Group]
+
+This returns a list of the groups that the specified user is a member of
+or an empty list if the user isn't in any groups.
+
+=head1 Unix::Groups::Group
+
+This is the class that represents the groups returned by the above methods.
+It stringifies to the group name and numifies to the group id.
+
+It has attributes that reflect the fields in C</etc/group>
+
+=head2 gid
+
+The L<Int> id of the group.
+
+=head2 name
+
+The name of the group.
+
+=head2 password
+
+The password for the group if set, most modern systems place this in a
+shadow file so this may be empty or some other meaningless value.
+
+=head2 users
+
+This is a list of the names of the users that are members of the group.
+
+=end pod
+
 class Unix::Groups {
 
     constant GROUPFILE = '/etc/group';
